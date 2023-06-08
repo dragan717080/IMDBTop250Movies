4e97f5ea18b1529af6b7b512be6e9ef264c9ad0f
= <hardlane2000@gmail.com>
2023-06-08 05:04:18 +0200

    adding the backend

---
+++ api/server.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const bodyParser = require('body-parser');
+app.use(cors());
+app.use(require('body-parser').urlencoded({ extended: false }));
+app.use(express.text())
+app.use(bodyParser.text({ type: '*/*' }));
+
+const PORT = 5000;
+const SQLConfig = require('./config/SQLConfig');
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({ SQLConfig });
+
+try {
+    connection.connect();
+} catch (err) {
+    console.error(err);
+}
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    connection.query('SELECT * FROM movies', function (error, results) {
+        if (error) throw error;
+        res.send(results);
+    });
+})
+
+app.get('/genres', async (req, res) => {
+    connection.query('SELECT genre_id as genreId, name FROM genres', function (error, results) {
+        if (error) throw error;
+        res.send(results);
+    });
+})
+
+app.get('/movies', async (req, res) => {
+    connection.query('SELECT * FROM movies', function (error, results) {
+        if (error) throw error;
+        res.send(results);
+    });
+})
+
+app.post('/movies', async (req, res) => {
+    connection.query(`SELECT * FROM movies WHERE genres REGEXP '${JSON.parse(req.body)}'`, function (error, results) {
+        if (error) throw error;
+        res.send(results);
+    });
+})
+
+app.get('/movies/:id', async (req, res) => {
+    const query1 = `SELECT * FROM movies_additional_details AS a JOIN movies AS b ON a.id = b.id WHERE a.id = ${req.params.id}`;
+
+    const result = await new Promise((resolve, reject) => {
+        connection.query(query1, (error, results) => {
+            if (error) reject(error);
+            else resolve(results[0]);
+        }
+        );
+    });
+
+    const productionCompanies = result.production_companies.split(',');
+    var logos = await Promise.all(
+        productionCompanies.map(async (productionCompanyId) => {
+            return new Promise((resolve, reject) => {
+                connection.query(`SELECT logo FROM production_companies WHERE id = ${productionCompanyId}`, (error, results) => {
+                    if (error) reject(error);
+                    else {
+                        resolve(results[0].logo);
+                    }
+                });
+            });
+        })
+    );
+    logos = logos.filter(logo => logo);
+
+    result.production_companies = logos;
+    res.send(result);
+})
+
+app.listen(PORT, () => {
+    console.log('Server is running on port ' + PORT);
+})
